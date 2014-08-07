2106bae23efa835bf41fb63a8fd8f3f2a26b6cbf
Niket <patelniket.7@gmail.com>
2014-08-06 21:16:44 -0400

    git pull output message changes
    Adding this output message to test git pull in eclipse.

--- GitNiket/src/de/vogella/git/first/GitTest.java
+++ GitNiket/src/de/vogella/git/first/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Git is cool!. I can work from anywhere now.");
+		System.out.println("Adding this output message to test git pull in eclipse.");
 
 	}
 
